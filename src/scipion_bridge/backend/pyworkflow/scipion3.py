3bf8020ac21dbe1a0a94543902d69e4d28727127
Maximilian Zoech <maximilian.zoech@cnb.csic.es>
2026-07-17 11:52:46 +0000

    Added support for enum type

--- src/scipion_bridge/backend/pyworkflow/scipion3.py
+++ src/scipion_bridge/backend/pyworkflow/scipion3.py
@@ -4,29 +4,33 @@ import pickle
 from functools import partial
 
 from ...core.protocol import Protocol, Field
-from typing import Type, get_origin, get_args
+from typing import get_args, Dict, Any
+
+from enum import Enum
 
 
 def convert_protocol_to_scipion3_protocol(
     protocol: Protocol,
     *,
     label: str,
-    conda_env: str,    
+    conda_env: str,
 ):
     try:
-        import pwem # type: ignore
-        from pwem.protocols import ProtProcessParticles, ProtFlexBase # type: ignore
-        from pwem.objects import SetOfParticles, SetOfParticlesFlex, ParticleFlex, SetOfVolumes, Volume # type: ignore
-        from pyworkflow.constants import BETA, Enum # type: ignore
-        from pyworkflow.plugin import Domain # type: ignore
-        from pwem.constants import ALIGN_PROJ, ALIGN_NONE # type: ignore
-        import pyworkflow.protocol.params as params # type: ignore
+        import pwem  # type: ignore
+        from pwem.protocols import ProtProcessParticles, ProtFlexBase  # type: ignore
+        from pwem.objects import SetOfParticles, SetOfParticlesFlex, ParticleFlex, SetOfVolumes, Volume  # type: ignore
+        from pyworkflow.constants import BETA  # type: ignore
+        from pyworkflow.plugin import Domain  # type: ignore
+        from pwem.constants import ALIGN_PROJ, ALIGN_NONE  # type: ignore
+        import pyworkflow.protocol.params as params  # type: ignore
     except ImportError:
-        raise ImportError("Using scipion bridge with scipion requires pyworkflow option. Install it using pip install \"scipion-bridge[pyworkflow]\"")
+        raise ImportError(
+            'Using scipion bridge with scipion requires pyworkflow option. Install it using pip install "scipion-bridge[pyworkflow]"'
+        )
 
-    def _get_param_type_and_kwargs(dtype):
+    def _get_param_type_and_kwargs(element: Field, dtype):
         param_type = params.StringParam
-        kwargs = {}
+        kwargs: Dict[str, Any] = {}
 
         if dtype is int:
             param_type = params.IntParam
@@ -36,10 +40,24 @@ def convert_protocol_to_scipion3_protocol(
             param_type = params.BooleanParam
         elif dtype is str:
             param_type = params.StringParam
+        elif issubclass(dtype, Enum):
+            param_type = params.EnumParam
+            choices = [option.value for option in dtype]
+
+            kwargs = {
+                "choices": choices,
+                "default": (
+                    choices.index(element.default) if element.default in choices else 0
+                ),
+            }
         else:
             # Check if it matches or inherits from a Scipion object class
-            raise NotImplementedError
-            
+            raise NotImplementedError(
+                f"The type {dtype} is not supported in the Scipion 3 backend."
+            )
+
+        kwargs.setdefault("default", element.default)
+
         return param_type, kwargs
 
     class ScipionProtocolWrapper(ProtProcessParticles, ProtFlexBase):
@@ -56,34 +74,32 @@ def convert_protocol_to_scipion3_protocol(
             form.addSection(label="Input")
             for key, element in protocol.configuration.inputs.items():
                 dtype = get_args(protocol._configuration.inputs[key])[0]
-                param, args = _get_param_type_and_kwargs(dtype)
-            
+                param, args = _get_param_type_and_kwargs(element, dtype)
+
                 form.addParam(
                     key,
                     param,
-                    default=element.default,
                     label=element.label if element.label is not None else key,
                     help=element.help,
-                    **args
+                    **args,
                 )
 
             form.addSection(label="Parameters")
             for key, element in protocol.configuration.parameters.items():
                 dtype = get_args(protocol._configuration.parameters[key])[0]
-                param, args = _get_param_type_and_kwargs(dtype)
+                param, args = _get_param_type_and_kwargs(element, dtype)
 
                 form.addParam(
                     key,
-                    params.IntParam,
-                    default=element.default,
+                    param,
                     label=element.label if element.label is not None else key,
                     help=element.help,
                     **args,
                 )
-            
 
         def _insertAllSteps(self):
             import logging
+
             logging.basicConfig(level=logging.DEBUG)
 
             print("Start inserting steps here")
@@ -95,7 +111,9 @@ def convert_protocol_to_scipion3_protocol(
             #     packages=["scipion_bridge"],
             # )
 
-    # Copy the module from the source protocol so Scipion class registration finds it
+    # Copy the module and class name from the source protocol so Scipion class registration finds it
     ScipionProtocolWrapper.__module__ = protocol.__module__
+    ScipionProtocolWrapper.__name__ = protocol.__class__.__name__
+    ScipionProtocolWrapper.__qualname__ = protocol.__class__.__name__
 
     return ScipionProtocolWrapper
